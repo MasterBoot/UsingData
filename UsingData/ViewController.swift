5361c2eb405825d99ea5bc7f7e3cfa37a7a4f81a
MasterBoot <1055258439@qq.com>
2016-04-18 21:23:57 +0800

    Sqlite使用
    创建表，插入数据测试成功！

--- UsingData/ViewController.swift
+++ UsingData/ViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        let row:AnyObject = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context);
+        row.setValue("shujuku1", forKey: "name");
+        row.setValue(1, forKey: "age");
+        
+        do {
+            try context.save()
+            print("保存成功！")
+        } catch {
+            fatalError("不能保存：\(error)")
+        }
+        print("run here");
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
