f3414f02a5adca32559d37e855678a48e526409d
MasterBoot <1055258439@qq.com>
2016-04-21 11:54:20 +0800

     coredate写入并读取数据
    用coredate创建数据表，并在程序中读取

--- UsingData/ViewController.swift
+++ UsingData/ViewController.swift
@@ -10,22 +10,35 @@ import UIKit
 import CoreData
 class ViewController: UIViewController {
 
+    var dateVar:Array<AnyObject>=[]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
         let row:AnyObject = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context);
         row.setValue("shujuku1", forKey: "name");
         row.setValue(1, forKey: "age");
-        
         do {
-            try context.save()
-            print("保存成功！")
+            try
+                context.save();
+            print("保存成功！");
+                //dateVar=context.executeFetchRequest(f);
         } catch {
             fatalError("不能保存：\(error)")
         }
         print("run here");
+        
+        
+        let f = NSFetchRequest(entityName: "Users");
+        do {
+            try dateVar=context.executeFetchRequest(f);
+            
+        }catch {
+            fatalError("错误：\(error)")
+        }
+        let name = dateVar[1].valueForKey("name");
+        let age  = dateVar[1].valueForKey("age");
+        print("\(name)");
+        print("\(age)");
 }
 
     override func didReceiveMemoryWarning() {
